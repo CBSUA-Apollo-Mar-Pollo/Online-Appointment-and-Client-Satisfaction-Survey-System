0866b58a07802eca59d1f7bcbe578ccabd2a6a40
veancaaa <veancamae.gutierrez@cbsua.edu.ph>
2022-04-22 21:02:00 +0900

    Initial commit

---
+++ src/views/MasterView.vue
@@ -0,0 +1,33 @@
+<template>
+  <!-- Start main -->
+  <main id="main">
+    <!-- ======= Appointment Section ======= -->
+    <AppointmentPage></AppointmentPage>
+    <!-- End Appointment Section -->
+
+    <!-- ======= Survey Section ======= -->
+    <SurveyPage></SurveyPage>
+    <!-- End Survey Section -->
+  </main>
+  <!-- End main -->
+
+  <!-- ======= Footer ======= -->
+  <PageFooter></PageFooter>
+  <!-- End Footer -->
+</template>
+
+<script>
+// @ is an alias to /src
+import AppointmentPage from "./pages/AppointmentPage.vue";
+import SurveyPage from "./pages/SurveyPage.vue";
+import PageFooter from "./pages/PageFooter.vue";
+
+export default {
+  name: "MasterView",
+  components: {
+    AppointmentPage,
+    SurveyPage,
+    PageFooter,
+  },
+};
+</script>
---
+++ src/views/pages/PageFooter.vue
@@ -0,0 +1,11 @@
+<template>
+  <footer id="footer">
+    <div class="container">
+      <h3>Lorem Ipsum</h3>
+      <p>
+        Et aut eum quis fuga eos sunt ipsa nihil. Labore corporis magni eligendi
+        fuga maxime saepe commodi placeat.
+      </p>
+    </div>
+  </footer>
+</template>

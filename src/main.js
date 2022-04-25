017f8efa5ffdb7ac443b91ca25927f63b1e14c32
Rin-Monjardin <apollomar22@gmail.com>
2022-04-25 18:54:02 +0800

    second commit

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
 import "./assets/tailwind.css";
+import "./assets/main.css";
 
 createApp(App).use(store).use(router).mount("#app");
 createApp(NavBar).use(store).use(router).mount("#NavBar");

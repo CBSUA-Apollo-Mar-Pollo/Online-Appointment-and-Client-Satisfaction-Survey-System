738b44e71010c75a5d5be6659682ca46d863140f
veancaaa <veancamae.gutierrez@cbsua.edu.ph>
2022-04-28 21:18:15 +0900

    Move file

--- src/main.js
+++ src/main.js
@@ -4,8 +4,8 @@ import NavBar from "./components/NavBar";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
-import "./assets/tailwind.css";
 import "./assets/main.css";
+import "./assets/style.css";
 import "./assets/tailwind.css";
 
 createApp(App).use(store).use(router).mount("#app");

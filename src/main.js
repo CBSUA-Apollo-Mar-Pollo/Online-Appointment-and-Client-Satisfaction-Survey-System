5c22fd92ba5f618d6ff76d5604f768820eeb7a0c
veancaaa <veancamae.gutierrez@cbsua.edu.ph>
2022-04-28 20:32:07 +0900

    Move file

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import NavBar from "./NavBar";
+import NavBar from "./components/NavBar";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";

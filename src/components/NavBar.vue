cb631506b0a81c70f19556c98cae15b758945e28
veancaaa <veancamae.gutierrez@cbsua.edu.ph>
2022-04-28 22:22:09 +0900

    Fix NavBar.vue

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,20 +1,22 @@
 <template>
-  <div class="TopNavbar container">
-    <nav
-      id="tnav1"
-      class="container flex justify-around py-3 mx-auto bg-sky-300"
-    >
-      <div>
+  <div class="TopNavbar w-100">
+    <div class="bg-sky-300 w-100 p-3">
+      <div id="tnav1" class="container d-flex py-3 mx-auto">
         <img id="timage" src="img/CHED-LOGO.png" alt="CHED LOGO" align="left" />
       </div>
-    </nav>
-    <nav id="tnav2" class="container flex justify-around py-5 mx-auto bg-white">
-      <div id="tnavcontent" class="space-x-8 text-black">
-        <a href="#WelcomePage">HOME</a>
-        <a href="#AppointmentPage">APPOINTMENT</a>
-        <a href="#SurveyPage">LOGIN</a>
-      </div>
-    </nav>
+    </div>
+    <div>
+      <nav
+        id="tnav2"
+        class="container flex justify-around py-5 mx-auto bg-white"
+      >
+        <div id="tnavcontent" class="space-x-8 text-black">
+          <a href="#WelcomePage">HOME</a>
+          <a href="#AppointmentPage">APPOINTMENT</a>
+          <a href="#SurveyPage">LOGIN</a>
+        </div>
+      </nav>
+    </div>
   </div>
 </template>
 

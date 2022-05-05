5030cb490d72ee3dc3f088befa8313c62d0fb445
veancaaa <veancamae.gutierrez@cbsua.edu.ph>
2022-05-05 13:12:40 +0800

    Add new pages

---
+++ src/views/AppointmentStatus.vue
@@ -0,0 +1,176 @@
+<template>
+  <div class="container">
+    <div class="animation-ctn">
+      <br />
+      <div class="section-title p-0">
+        <h2 class="ref">Check Service Request Status</h2>
+      </div>
+      <p class="mt-2">
+        Please enter below the request number you received to view <br />
+        the status of your appointment request:
+      </p>
+
+      <form>
+        <div>
+          <input type="text" class="p-1 mt-3 border-2" id="text2" required />
+        </div>
+        <input
+          type="button"
+          class="col-2 btn bg-primary text-white mt-3 button"
+          value="Submit"
+          @click="showAlert"
+        />
+      </form>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  box-shadow: 0 2px 5px rgb(175, 175, 175);
+  padding: 5px;
+  width: 50%;
+  margin-top: 30px;
+}
+
+.bg-light {
+  color: #008aff;
+}
+
+.section-title h2 {
+  color: #008aff;
+}
+
+.ref {
+  margin: 5px 0 1px 0;
+}
+
+.number {
+  font-size: 50px;
+  font-weight: 700px;
+  position: relative;
+  color: #45505b;
+  margin: 15px;
+}
+
+.alert {
+  background: #f8d7da;
+  padding: 5px 10px;
+  border-radius: 8px;
+}
+.animation-ctn {
+  text-align: center;
+  margin: 5em auto;
+}
+
+@-webkit-keyframes checkmark {
+  0% {
+    stroke-dashoffset: 100px;
+  }
+
+  100% {
+    stroke-dashoffset: 200px;
+  }
+}
+
+@-ms-keyframes checkmark {
+  0% {
+    stroke-dashoffset: 100px;
+  }
+
+  100% {
+    stroke-dashoffset: 200px;
+  }
+}
+
+@keyframes checkmark {
+  0% {
+    stroke-dashoffset: 100px;
+  }
+
+  100% {
+    stroke-dashoffset: 0px;
+  }
+}
+
+@-webkit-keyframes checkmark-circle {
+  0% {
+    stroke-dashoffset: 480px;
+  }
+
+  100% {
+    stroke-dashoffset: 960px;
+  }
+}
+
+@-ms-keyframes checkmark-circle {
+  0% {
+    stroke-dashoffset: 240px;
+  }
+
+  100% {
+    stroke-dashoffset: 480px;
+  }
+}
+
+@keyframes checkmark-circle {
+  0% {
+    stroke-dashoffset: 480px;
+  }
+
+  100% {
+    stroke-dashoffset: 960px;
+  }
+}
+
+@keyframes colored-circle {
+  0% {
+    opacity: 0;
+  }
+
+  100% {
+    opacity: 100;
+  }
+}
+
+.inlinesvg .svg svg {
+  display: inline;
+}
+
+.icon--order-success svg polyline {
+  -webkit-animation: checkmark 0.3s ease-in-out 0.9s backwards;
+  animation: checkmark 0.3s ease-in-out 0.9s backwards;
+}
+
+.icon--order-success svg circle {
+  -webkit-animation: checkmark-circle 0.6s ease-in-out backwards;
+  animation: checkmark-circle 0.6s ease-in-out backwards;
+}
+.icon--order-success svg circle#colored {
+  -webkit-animation: colored-circle 0.6s ease-in-out 0.7s backwards;
+  animation: colored-circle 0.6s ease-in-out 0.7s backwards;
+}
+</style>
+
+<script>
+export default {
+  methods: {
+    showAlert() {
+      // Use sweetalert2
+      this.$swal
+        .fire({
+          title: "Processed",
+          text: "",
+          icon: "success",
+          showCancelButton: false,
+          confirmButtonColor: "#3085d6",
+          cancelButtonColor: "#d33",
+          confirmButtonText: "Yes",
+        })
+        .then(function () {
+          window.location = "/SurveyForm";
+        });
+    },
+  },
+};
+</script>
---
+++ src/views/ReferenceNumber.vue
@@ -0,0 +1,186 @@
+<template>
+  <div class="container">
+    <div class="animation-ctn">
+      <div class="icon icon--order-success svg justify-around d-flex">
+        <svg xmlns="http://www.w3.org/2000/svg" width="154px" height="154px">
+          <g fill="none" stroke="#22AE73" stroke-width="2">
+            <circle
+              cx="77"
+              cy="77"
+              r="72"
+              style="stroke-dasharray: 480px, 480px; stroke-dashoffset: 960px"
+            ></circle>
+            <circle
+              id="colored"
+              fill="#22AE73"
+              cx="77"
+              cy="77"
+              r="72"
+              style="stroke-dasharray: 480px, 480px; stroke-dashoffset: 960px"
+            ></circle>
+            <polyline
+              class="st0"
+              stroke="#fff"
+              stroke-width="10"
+              points="43.5,77.8 63.7,97.9 112.2,49.4 "
+              style="stroke-dasharray: 100px, 100px; stroke-dashoffset: 200px"
+            />
+          </g>
+        </svg>
+      </div>
+      <br />
+      <div class="section-title p-0">
+        <h2 class="ref">Reference ID</h2>
+      </div>
+      <p>To see your appointment status, copy the request number below</p>
+      <h2 class="number">12345</h2>
+      <div class="text-center">
+        <a
+          class="col-2 btn bg-light"
+          href="/ReferenceNumber"
+          role="button"
+          id="submit"
+          type="submit"
+        >
+          Copy </a
+        ><br />
+        <a
+          class="col-2 btn bg-primary text-white mt-1"
+          href="/AppointmentStatus"
+          role="button"
+          id="submit"
+          type="submit"
+        >
+          Next
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  box-shadow: 0 2px 5px rgb(175, 175, 175);
+  padding: 5px;
+  width: 50%;
+  margin-top: 30px;
+}
+
+.bg-light {
+  color: #008aff;
+}
+
+.section-title h2 {
+  color: #008aff;
+}
+
+.ref {
+  margin: 5px 0 1px 0;
+}
+
+.number {
+  font-size: 50px;
+  font-weight: 700px;
+  position: relative;
+  color: #45505b;
+  margin: 15px;
+}
+
+.alert {
+  background: #f8d7da;
+  padding: 5px 10px;
+  border-radius: 8px;
+}
+.animation-ctn {
+  text-align: center;
+  margin: 5em auto;
+}
+
+@-webkit-keyframes checkmark {
+  0% {
+    stroke-dashoffset: 100px;
+  }
+
+  100% {
+    stroke-dashoffset: 200px;
+  }
+}
+
+@-ms-keyframes checkmark {
+  0% {
+    stroke-dashoffset: 100px;
+  }
+
+  100% {
+    stroke-dashoffset: 200px;
+  }
+}
+
+@keyframes checkmark {
+  0% {
+    stroke-dashoffset: 100px;
+  }
+
+  100% {
+    stroke-dashoffset: 0px;
+  }
+}
+
+@-webkit-keyframes checkmark-circle {
+  0% {
+    stroke-dashoffset: 480px;
+  }
+
+  100% {
+    stroke-dashoffset: 960px;
+  }
+}
+
+@-ms-keyframes checkmark-circle {
+  0% {
+    stroke-dashoffset: 240px;
+  }
+
+  100% {
+    stroke-dashoffset: 480px;
+  }
+}
+
+@keyframes checkmark-circle {
+  0% {
+    stroke-dashoffset: 480px;
+  }
+
+  100% {
+    stroke-dashoffset: 960px;
+  }
+}
+
+@keyframes colored-circle {
+  0% {
+    opacity: 0;
+  }
+
+  100% {
+    opacity: 100;
+  }
+}
+
+.inlinesvg .svg svg {
+  display: inline;
+}
+
+.icon--order-success svg polyline {
+  -webkit-animation: checkmark 0.3s ease-in-out 0.9s backwards;
+  animation: checkmark 0.3s ease-in-out 0.9s backwards;
+}
+
+.icon--order-success svg circle {
+  -webkit-animation: checkmark-circle 0.6s ease-in-out backwards;
+  animation: checkmark-circle 0.6s ease-in-out backwards;
+}
+.icon--order-success svg circle#colored {
+  -webkit-animation: colored-circle 0.6s ease-in-out 0.7s backwards;
+  animation: colored-circle 0.6s ease-in-out 0.7s backwards;
+}
+</style>
